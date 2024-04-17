9e31330f96aea485e42ccb88e879976772bdfa26
janu1499 <januja@apptimustech.com>
2024-04-17 16:49:37 +0530

    changes and bug fixed

--- lib/accessible_heading_text.dart
+++ lib/accessible_heading_text.dart
@@ -1,27 +1,71 @@
+import 'package:accessibility_features/accessibility_features.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:accessibility_features/accessibility_features.dart';
 
-class AccessibleHeadingText extends StatelessWidget {
+class AccessibleHeadingText extends StatefulWidget {
   final String data;
   final TextStyle? style;
 
-  const AccessibleHeadingText(this.data, {super.key, this.style,});
+  const AccessibleHeadingText(
+    this.data, {
+    super.key,
+    this.style,
+  });
+
+  @override
+  State<AccessibleHeadingText> createState() => _AccessibleHeadingTextState();
+}
+
+class _AccessibleHeadingTextState extends State<AccessibleHeadingText> {
+  AccessibilityFeatures accessibilitySettings = AccessibilityFeatures();
+  late Color headingColor;
+
+  void initState() {
+    super.initState();
+    // Initialize heading color here
+    // headingColor = widget.style?.color ?? AccessibilityFeatures().headingColor;
+    accessibilitySettings.setHeadingColor(
+        widget.style?.color ?? AccessibilityFeatures().headingColor);
+  }
 
   @override
   Widget build(BuildContext context) {
     final accessibilitySettings = context.watch<AccessibilityFeatures>();
+    final double minFontSize = 16.0;
+    final double maxFontSize = 24.0;
+    final double minLineSpace = 0.5;
+    final double maxLineSpace = 3.0;
+
+    final Color? textColor = widget.style?.color;
+    final Color fallbackColor = accessibilitySettings.headingColor;
+    final bool isBlackOrWhite =
+        textColor == Colors.black || textColor == Colors.white;
+
+    final Color finalColor =
+        isBlackOrWhite ? fallbackColor : textColor ?? fallbackColor;
 
     return Align(
       alignment: accessibilitySettings.textAlignment,
       child: Text(
-        data,
-        style: (style ?? const TextStyle()).copyWith(
-          fontWeight: accessibilitySettings.impairedMode ? FontWeight.bold : FontWeight.normal,
-          backgroundColor: accessibilitySettings.textBgColor,
-          fontSize: accessibilitySettings.currentFontSize* accessibilitySettings.textScaleFactor* (accessibilitySettings.impairedMode ? 1.2 : 1) ,
-          color: accessibilitySettings.headingColor,
-          height: accessibilitySettings.lineHeight,
+        widget.data,
+        style: (widget.style ?? const TextStyle()).copyWith(
+          fontWeight: widget.style?.fontWeight ??
+              (accessibilitySettings.impairedMode
+                  ? FontWeight.bold
+                  : FontWeight.normal),
+          backgroundColor: widget.style?.backgroundColor ??
+              accessibilitySettings.textBgColor,
+          fontSize: ((widget.style?.fontSize ?? 0) *
+                      accessibilitySettings.textScaleFactor *
+                      (accessibilitySettings.impairedMode ? 1.2 : 1))
+                  .clamp(minFontSize, maxFontSize) ??
+              (accessibilitySettings.currentFontSize *
+                      accessibilitySettings.textScaleFactor *
+                      (accessibilitySettings.impairedMode ? 1.2 : 1))
+                  .clamp(minFontSize, maxFontSize),
+          color: finalColor,
+          height: accessibilitySettings.lineHeight
+              .clamp(minLineSpace, maxLineSpace),
           letterSpacing: accessibilitySettings.letterSpacing,
         ),
       ),
--- lib/accessible_text.dart
+++ lib/accessible_text.dart
@@ -1,28 +1,53 @@
+import 'package:accessibility_features/accessibility_features.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:accessibility_features/accessibility_features.dart';
 
 class AccessibleText extends StatelessWidget {
   final String data;
   final TextStyle? style;
-  
 
   const AccessibleText(this.data, {super.key, this.style});
+
+  final double minFontSize = 12.0;
+
+  final double maxFontSize = 20.0;
+  final double minLineSpace = 0.5;
+  final double maxLineSpace = 3.0;
 
   @override
   Widget build(BuildContext context) {
     final accessibilitySettings = context.watch<AccessibilityFeatures>();
+
+    final Color? textColor = style?.color;
+    final Color fallbackColor = accessibilitySettings.textColor;
+    final bool isBlackOrWhite =
+        textColor == Colors.black || textColor == Colors.white;
+
+    final Color finalColor =
+        isBlackOrWhite ? fallbackColor : textColor ?? fallbackColor;
 
     return Align(
       alignment: accessibilitySettings.textAlignment,
       child: Text(
         data,
         style: (style ?? const TextStyle()).copyWith(
-          fontWeight: accessibilitySettings.impairedMode ? FontWeight.bold : FontWeight.normal,
-          backgroundColor: accessibilitySettings.textBgColor,
-          fontSize: accessibilitySettings.currentFontSize* accessibilitySettings.textScaleFactor ,
-          color:  accessibilitySettings.textColor,
-          height: accessibilitySettings.lineHeight,
+          fontWeight: style?.fontWeight ??
+              (accessibilitySettings.impairedMode
+                  ? FontWeight.bold
+                  : FontWeight.normal),
+          backgroundColor:
+              style?.backgroundColor ?? accessibilitySettings.textBgColor,
+          fontSize: ((style?.fontSize ?? 0) *
+                      accessibilitySettings.textScaleFactor *
+                      (accessibilitySettings.impairedMode ? 1.2 : 1))
+                  .clamp(minFontSize, maxFontSize) ??
+              (accessibilitySettings.currentFontSize *
+                      accessibilitySettings.textScaleFactor *
+                      (accessibilitySettings.impairedMode ? 1.2 : 1))
+                  .clamp(minFontSize, maxFontSize),
+          color: finalColor,
+          height: accessibilitySettings.lineHeight
+              .clamp(minLineSpace, maxLineSpace),
           letterSpacing: accessibilitySettings.letterSpacing,
         ),
       ),
